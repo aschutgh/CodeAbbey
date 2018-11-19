834a45cfecf1224d0be2c295a821299624d2cbfc
Arthur Schut <aschutgh@gmail.com>
2018-11-19 10:47:46 +0100

    Problem N3 solved

---
+++ ProblemN3SumsInLoop/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProblemN3SumsInLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter number of pairs to sum: ");
+            int nofp = int.Parse(Console.ReadLine());
+            List<string> ptos = new List<string>();
+            for (int i = 0; i < nofp; i++)
+            {
+                ptos.Add(Console.ReadLine());
+            }
+
+            foreach(string s in ptos)
+            {
+                string[] tokens = s.Split(" ");
+                Console.WriteLine(int.Parse(tokens[0]) + int.Parse(tokens[1]));
+            }
+        }
+    }
+}
