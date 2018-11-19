7e869987e2814b1797de9c82cfe19b85520fcd5d
Arthur Schut <aschutgh@gmail.com>
2018-11-19 13:47:44 +0100

    Solved Problem N11

---
+++ ProblemN11SumOfDigits/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+//Input data are in the following format:
+
+//first line contains N - the number of values to process;
+//and then N lines will follow describing the values for which sum of digits should be calculated by 3 integers A B C;
+//for each case you need to multiply A by B and add C(i.e.A* B + C) - then calculate sum of digits of the result.
+
+//Answer should have N results, also separated by spaces. For example:
+
+
+namespace ProblemN11SumOfDigits
+{
+    class Program
+    {
+        static int SumOfDigits(int number)
+        {
+            int a = number;
+            int sum = 0;
+            while (a > 0)
+            {
+                sum += a % 10;
+                a /= 10;
+            }
+            return sum;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Enter number of lines with values: ");
+            int nol = int.Parse(Console.ReadLine());
+            Console.Write("Enter Values: ");
+            List<string> values = new List<string>();
+            for (int i = 0; i < nol; i++)
+            {
+                values.Add(Console.ReadLine());
+            }
+
+            foreach(string s in values)
+            {
+                string[] tokens = s.Split(" ");
+                int a = int.Parse(tokens[0]);
+                int b = int.Parse(tokens[1]);
+                int c = int.Parse(tokens[2]);
+                Console.Write("{0} ", SumOfDigits((a * b) + c));
+            }
+        }
+    }
+}
---
+++ ProblemN7FahrenheitToCelsius/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+//You are to write program to convert degrees of Fahrenheit to Celsius.
+
+//Input data contains N+1 values, first of them is N itself (Note that you should not try to convert it).
+//Answer should contain exactly N results, rounded to nearest integer and separated by spaces.
+
+
+namespace ProblemN7FahrenheitToCelsius
+{
+    class Program
+    {
+        static int FtoC (int TempInF)
+        {
+            double F = TempInF;
+            double C = (F - 32.0) / 1.8;
+            return (int)Math.Round(C); 
+        }
+
+        static void Main(string[] args)
+        {
+            string[] tempFvalues;
+            Console.WriteLine("Enter temperatures in fahrenheit separated by spaces. The first value should be the number of temperatures.");
+            Console.Write("> ");
+            tempFvalues = Console.ReadLine().Split(" ");
+
+            for(int i = 1; i < int.Parse(tempFvalues[0])+1; i++)
+            {
+                int TempInC = FtoC(int.Parse(tempFvalues[i]));
+                Console.Write("{0} ", TempInC);
+            }
+        }
+    }
+}
