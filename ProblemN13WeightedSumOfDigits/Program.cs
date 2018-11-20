cd22b486361a655ae68ace8889c21812bd727d98
Arthur Schut <aschutgh@gmail.com>
2018-11-20 13:15:12 +0100

    Solved problem 13

---
+++ ProblemN13WeightedSumOfDigits/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ProblemN13WeightedSumOfDigits
+{
+    class Program
+    {
+        static int Wsd(string number)
+        {
+            int ws = 0;
+
+            for(int i = 0; i < number.Length; i++)
+            {
+                ws += (i + 1) * int.Parse(number.Substring(i, 1));
+            }
+
+            return ws;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Number of digits to be processed: ");
+            int nod = int.Parse(Console.ReadLine());
+
+            string[] numbers = Console.ReadLine().Split(" ");
+            for(int i = 0; i < nod; i++)
+            {
+                Console.Write("{0} ", Wsd(numbers[i]));
+            }
+        }
+    }
+}
