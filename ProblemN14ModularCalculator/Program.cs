58c938222f3ae197e6b23470d6562d2181b8441b
Arthur Schut <aschutgh@gmail.com>
2018-11-20 14:55:32 +0100

    Solved problem 14

---
+++ ProblemN14ModularCalculator/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+
+namespace ProblemN14ModularCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter initial number: ");
+            int initNumber = int.Parse(Console.ReadLine());
+            List<string> lines = new List<string>();
+
+            bool next = true;
+
+            while(next)
+            {
+                string line = Console.ReadLine();
+                if (line != "")
+                {
+                    lines.Add(line);
+                    if(line.Substring(0, 1) == "%")
+                    {
+                        next = false;
+                    }
+                }
+            }
+
+            BigInteger result = new BigInteger(initNumber);
+
+            foreach(string s in lines)
+            {
+                string[] tokens;
+                tokens = s.Split(" ");
+                switch (tokens[0])
+                 {
+                    case "+":
+                        result = result + int.Parse(tokens[1]);
+                        break;
+                    case "*":
+                        result = result * int.Parse(tokens[1]);
+                        break;
+                    case "%":
+                        result = result % int.Parse(tokens[1]);
+                        break;
+                    default:
+                        break;
+                 }
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
