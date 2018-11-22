19edfab40701067af9b296523b7ee486c3251cec
Arthur Schut <aschutgh@gmail.com>
2018-11-22 13:22:55 +0100

    Started on problem 17

---
+++ ProblemN17ArrayChecksum/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProblemN17ArrayChecksum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
