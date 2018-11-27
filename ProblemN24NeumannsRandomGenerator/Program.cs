914e5387bcbe5ec6e46e3318425ad9c381ff6566
Arthur Schut <aschutgh@gmail.com>
2018-11-27 11:21:49 +0100

    start over with problem 24

---
+++ ProblemN24NeumannsRandomGenerator/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+// FIXME: start over
+
+namespace ProblemN24NeumannsRandomGenerator
+{
+    class Program
+    {
+        static int NRG(int n)
+        {
+            int square = n * n;
+            string sr = square.ToString();
+            int srl = sr.Length;
+            if (sr.Length < 8)
+            {
+                string lz = string("0", 8 - srl);
+                sr = lz + sr;
+            }
+
+
+        }
+    
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
