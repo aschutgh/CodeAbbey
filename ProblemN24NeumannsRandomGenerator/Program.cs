0c4b9a91c440dd48e6f81a01e6e2492e04fd1d50
Arthur Schut <aschutgh@gmail.com>
2018-11-27 13:51:06 +0100

    Solved Problem 24

--- ProblemN24NeumannsRandomGenerator/Program.cs
+++ ProblemN24NeumannsRandomGenerator/Program.cs
@@ -1,28 +1,59 @@
 ﻿using System;
-
-// FIXME: start over
+using System.Collections.Generic;
 
 namespace ProblemN24NeumannsRandomGenerator
 {
     class Program
     {
-        static int NRG(int n)
+        static int Nrg(int n)
         {
             int square = n * n;
             string sr = square.ToString();
             int srl = sr.Length;
             if (sr.Length < 8)
             {
-                string lz = string("0", 8 - srl);
+                string lz = new string('0', 8 - srl);
                 sr = lz + sr;
             }
-
+            sr = sr.Substring(2, 4);
+            return int.Parse(sr);
 
         }
     
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Amount of initial values: ");
+            int nov = int.Parse(Console.ReadLine());
+            string[] values_str = Console.ReadLine().Split(" ");
+            int[] values_int = new int[nov];
+            for(int i = 0; i < nov; i++)
+            {
+                values_int[i] = int.Parse(values_str[i]);
+            }
+
+            for(int i = 0; i < nov; i++)
+            {
+                int result = Nrg(values_int[i]);
+                int ctr = 1;
+                HashSet<int> encountered = new HashSet<int>();
+                if (result != values_int[i])
+                {
+                    encountered.Add(values_int[i]);
+                    encountered.Add(result);
+                    ctr += 1;
+                    while(true)
+                    {
+                        if (!encountered.Contains(Nrg(result)))
+                        {
+                            result = Nrg(result);
+                            ctr += 1;
+                            encountered.Add(result);
+                        }
+                        else break;
+                    }
+                }
+                Console.Write("{0} ", ctr);
+            }
         }
     }
 }
